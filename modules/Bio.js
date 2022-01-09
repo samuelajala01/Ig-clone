e19f39bd846e319ca8aa26380e5d892c6495e6ef
Samuel Ajala <samuelajala01@gmail.com>
2022-01-09 18:29:40 +0100

    split sections into modules

---
+++ modules/Bio.js
@@ -0,0 +1,9 @@
+ const Bio = () => `
+  <section class="bio">
+  <div class="profile-photo"><img src="./images/kevin-hart.jfif" alt="profile-photo" /></div>
+  <div class="profile-info">
+    <p class="name">Jane Butters</p>
+    <button>Follow</button>
+  </div>
+  </section>
+`;
---
+++ modules/Gallery.js
@@ -0,0 +1,23 @@
+const Gallery = () => `
+  <div class="gallery-nav"></div>
+  <section class="gallery">
+    <a href="#" class="item">
+      <img src="./images/kevin-hart.jfif" alt="">
+    </a>
+    <a href="#" class="item">
+      <img src="./images/kevin-hart.jfif" alt="">
+    </a>
+    <a href="#" class="item">
+      <img src="./images/kevin-hart.jfif" alt="">
+    </a>
+    <a href="#" class="item">
+      <img src="./images/kevin-hart.jfif" alt="">
+    </a>
+    <a href="#" class="item">
+      <img src="./images/kevin-hart.jfif" alt="">
+    </a>
+    <a href="#" class="item">
+      <img src="./images/kevin-hart.jfif" alt="">
+    </a>
+  </section>
+`
