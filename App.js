790e8ca86ca5ac780c1feeb34109c6dbb1c779c8
Samuel Ajala <samuelajala01@gmail.com>
2022-01-18 16:04:57 +0100

    created edit name functionality

--- App.js
+++ App.js
@@ -12,9 +12,19 @@ const App = () =>{
   `
 }
 
+
 document.getElementById('root').innerHTML = App();
 
+const editBioForm = document.querySelector('.edit-bio-form')
 
+editBioForm.addEventListener('submit', (e) =>{
+  const nameInput = document.querySelector('#name').value
+  const nameOutput = document.querySelector('.name')
+  console.log(nameInput)
+  nameOutput.innerText = nameInput
+
+  e.preventDefault();
+})
 
 
 
--- modules/Bio.js
+++ modules/Bio.js
@@ -1,11 +1,17 @@
- const Bio = () => `
+ const Bio = () =>{
+ 
+ return `
   <section class="bio">
   <div class="profile-photo"><img src="./images/kevin-hart.jfif" alt="profile-photo" /></div>
   <div class="profile-info">
-    <p class="name">Jane Butters</p>
-    <button>Follow</button>
+    <p class="name">Samuel Ajala</p>
+    <button>Edit bio</button>
   </div>
+  <form class="edit-bio-form">
+  <input type="text" id="name" placeholder="Name"/>
+  <button id='submit' type="submit">Submit</button> 
+  </form>
   </section>
-`;
-
+`}
+  
 export default Bio;
